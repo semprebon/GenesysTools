997408640e94b3e721e154c6e27a5531dd1f0964
Semprebon <semprebon@gmail.com>
2020-01-16 15:40:03 -0500

    Add card generator; specify files for dark fantasy game

--- character_sheet.py
+++ character_sheet.py
@@ -3,15 +3,14 @@ import os
 import argparse
 from os import path
 
-from pyparsing import basestring
 from reportlab.lib import colors
-from reportlab.lib.colors import black, Color
+from reportlab.lib.colors import black, white, Color
 from reportlab.lib.enums import TA_CENTER, TA_RIGHT
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 from reportlab.platypus import SimpleDocTemplate, PageTemplate, Frame, BaseDocTemplate, Paragraph, HRFlowable, Image, \
-    PageBreak, Spacer, Table
+    PageBreak, Spacer, Table, Flowable
 
 
 def format_modifier(value):
@@ -34,6 +33,7 @@ class CharacterSheet:
     SIDEBAR_WIDTH = 3.0*inch
 
     NORMAL_STYLE = ParagraphStyle("normal", fontName="Times-Roman", fontSize=9, leading=10)
+    SKILL_STYLE = ParagraphStyle("normal", fontName="Times-Roman", fontSize=9, leading=10)
     TITLE_STYLE = ParagraphStyle("title", fontName="Helvetica", fontSize=11, leading=13, textColor=colors.darkblue)
     SECTION_HEADER_STYLE = ParagraphStyle("section_header", fontName="Helvetica", fontSize=9, leading=9, alignment=TA_CENTER)
     SIDEBAR_STYLE = ParagraphStyle("normal", fontName="Times-Roman", fontSize=9, leading=11, alignment=TA_RIGHT)
@@ -57,10 +57,10 @@ class CharacterSheet:
         from reportlab.pdfbase.ttfonts import TTFont
         #self.register_font_family('Arial')
         #self.register_font_family('Arial Narrow')
-        normal = pdfmetrics.registerFont(TTFont("Arial-Regular", 'Arial.ttf'))
-        bold = pdfmetrics.registerFont(TTFont('Arial-Bold', 'Arial Bold.ttf'))
-        pdfmetrics.registerFontFamily("Arial", normal=bold, bold=bold, italic=normal, boldItalic=bold)
-        genesysFont = pdfmetrics.registerFont(TTFont("genesys", 'genesys.ttf'))
+        normal = pdfmetrics.registerFont(TTFont("Arial", 'Arial.ttf'))
+        #bold = pdfmetrics.registerFont(TTFont('Arial-Bold', 'Arial Bold.ttf'))
+        #pdfmetrics.registerFontFamily("Arial", normal=bold, bold=bold, italic=normal, boldItalic=bold)
+        genesysFont = pdfmetrics.registerFont(TTFont("genesys", 'C:\\Users\\Semprebon\\Appdata\\local\\microsoft\\windows\\fonts\\GenesysGlyphsAndDice-2.2.ttf'))
 
 
     def prepare_page_fn(self, canvas, doc):
@@ -69,10 +69,15 @@ class CharacterSheet:
     def horizontal_line(self):
         return HRFlowable(width="100%", color=black, hAlign="CENTER")
 
-    def title(self, name, archetype, career):
+    def title(self, data):
         #return [Paragraph(text, self.TITLE_STYLE)]
-        return [Paragraph("<b>%s</b> (%s %s)" % (name, archetype, career), self.TITLE_STYLE), self.horizontal_line()]
-
+        archetype = self.resolve_reference(data['archetype'], data['setting']['archetypes'])
+        career = self.resolve_reference(data['career'], data['setting']['careers'])
+        return [Paragraph("<b>%s</b> (%s %s) %s" % (data["name"], archetype["name"], career["name"], data["player"]),
+                          self.TITLE_STYLE),
+                self.horizontal_line()]
+# {'name': 'Sharaf el-Basha',
+    # 'setting': {'skills': {'Alchemy': {'characteristic': 'INT', 'ref': 57, 'type': 'general'}, 'Athletics': {'characteristic': 'BR', 'ref': 58, 'type': 'general'}, 'Brawl': {'characteristic': 'BR', 'ref': 67, 'type': 'combat'}, 'Charm': {'characteristic': 'PR', 'ref': 54, 'type': 'social'}, 'Coercion': {'characteristic': 'WILL', 'ref': 55, 'type': 'social'}, 'Cool': {'characteristic': 'PR', 'ref': 59, 'type': 'general'}, 'Coordination': {'characteristic': 'AG', 'ref': 59, 'type': 'general'}, 'Deception': {'characteristic': 'CUN', 'ref': 56, 'type': 'social'}, 'Discipline': {'characteristic': 'WILL', 'ref': 60, 'type': 'general'}, 'Knowledge (Lore)': {'characteristic': 'INT', 'ref': 'RoT', 'type': 'knowledge'}, 'Leadership': {'characteristic': 'PR', 'ref': 56, 'type': 'social'}, 'Mechanics': {'characteristic': 'INT', 'ref': 61, 'type': 'general'}, 'Medicine': {'characteristic': 'INT', 'ref': 61, 'type': 'general'}, 'Melee (Heavy)': {'characteristic': 'BR', 'ref': 68, 'type': 'combat'}, 'Melee (Light)': {'characteristic': 'BR', 'ref': 68, 'type': 'combat'}, 'Negotiation': {'characteristic': 'PR', 'ref': 56, 'type': 'social'}, 'Operating': {'characteristic': 'INT', 'ref': 62, 'type': 'general'}, 'Perception': {'characteristic': 'CUN', 'ref': 62, 'type': 'general'}, 'Ranged': {'characteristic': 'AG', 'ref': 68, 'type': 'combat'}, 'Resilience': {'characteristic': 'BR', 'ref': 63, 'type': 'general'}, 'Riding': {'characteristic': 'AG', 'ref': 63, 'type': 'general'}, 'Skulduggery': {'characteristic': 'CUN', 'ref': 64, 'type': 'general'}, 'Sorcerty': {'characteristic': 'WILL', 'ref': 69, 'type': 'magic'}, 'Stealth': {'characteristic': 'AG', 'ref': 64, 'type': 'general'}, 'Streetwise': {'characteristic': 'CUN', 'ref': 65, 'type': 'general'}, 'Survival': {'characteristic': 'CUN', 'ref': 65, 'type': 'general'}, 'Thaumaturgy': {'characteristic': 'INT', 'ref': 'BPG', 'type': 'magic'}, 'Vigilance': {'characteristic': 'WILL', 'ref': 65, 'type': 'general'}, 'Witchcraft': {'characteristic': 'CUN', 'ref': 69, 'type': 'magic'}}, 'archetypes': {'Nomad': {'characteristics': {'brawn': 2, 'agilty': 2, 'intellect': 2, 'cunning': 3, 'willpower': 2, 'presence': 1}, 'wound_threshold': 10, 'strain_threshold': 10, 'experience': 100, 'starting_skills': ['survival'], 'Nomadâ€™s Dodge': 'Once per session your character may spend a Story Point as an out-of-turn incidental to add [SD] to a melee or ranged attack against them.'}, 'Leader': {'characteristics': {'brawn': 2, 'agilty': 1, 'intellect': 2, 'cunning': 2, 'willpower': 3, 'presence': 2}, 'wound_threshold': 8, 'strain_threshold': 12, 'experience': 100, 'starting_skills': ['Coercion'], 'Force of Will': 'Once per session, when you are making an Opposed Skill Check, treat the opponents Characteristic as Two and their Skill Ranks as Zero'}, 'Scholar': {'characteristics': {'brawn': 2, 'agilty': 1, 'intellect': 3, 'cunning': 2, 'willpower': 1, 'presence': 2}, 'wound_threshold': 8, 'strain_threshold': 12, 'experience': 100, 'starting_skills': ['any one knowledge'], 'Brilliant!': 'Once per session your character may spend a Story Point as an incidental. If they do so, during the next check they make during that turn, you count their ranks in the skill being used as equal to their Intellect.'}, 'Slave': {'characteristics': {'brawn': 3, 'agilty': 2, 'intellect': 2, 'cunning': 2, 'willpower': 1, 'presence': 2}, 'wound_threshold': 12, 'strain_threshold': 8, 'experience': 100, 'starting_skills': ['athletics'], 'tough_as_nails': 'Once per session, your character may spend a Story Point as an out-of-turn inciden- tal immediately after suffering a Critical Injury and determining the result. If they do so, they count the result rolled as â€œ01.'}}}, 'archetype': 'Leader', 'career': 'Alban Priest', 'player': 'Example', 'wound_threshold': 16, 'strain_threshold': 9, 'characteristics': {'BR': 3, 'AG': 1, 'INT': 2, 'CUN': 2, 'WILL': 4, 'PR': 3}, 'skills': {'Coercion': 2, 'Discipline': 1, 'Melee (Light)': 1, 'Sorcery': 1}, 'motivation': {'desire': {'name': 'Justice', 'text': 'Sharif follows a sect of Albanism that promotes equal rights and justice for all.'}, 'fear': {'name': 'Obscurity', 'text': 'Sharif wants to be remembered for his struggle on behalf of the down-trodden.'}, 'strength': {'name': 'Witty', 'text': "Sharif's idealism is tempered by his genial humor."}, 'flaw': {'name': 'Anger', 'text': 'Sharif has little patience for those who stand in the way of his objective.'}}, 'equipment': ['Holy Icon', 'Priests Vestments', 'Mace'], 'silver': 150}
     def ability_scores(self, data):
         codes = {"str": "S", "dex": "D", "con": "C", "wis": "W", "int": "I", "cha": "Ch"}
         scores = data["attributes"]
@@ -91,21 +96,54 @@ class CharacterSheet:
             Paragraph(" ".join([ "%s: %d" % item for item in data["characteristics"].items() ]), self.NORMAL_STYLE),
             self.horizontal_line() ]
 
-    PROFICIENCY_SYMBOL = "\uE90B"
-    ABILITY_SYMBOL = "\uE905"
+    #PROFICIENCY_SYMBOL = "\uE90B"
+    #ABILITY_SYMBOL = "\uE905"
+    PROFICIENCY_SYMBOL = "K"
+    ABILITY_SYMBOL = "L"
 
-    def format_skill(self, name, rank, characteristic, career_skills):
-        proficiencies = min(rank, characteristic)
-        abilities = max(rank, characteristic) - proficiencies
-        return[ Paragraph("<b>%s</b>" % name, self.NORMAL_STYLE),
-          Paragraph("%d" % rank, self.NORMAL_STYLE),
-          Paragraph("<font color='#fff201'>%s</font><font color='#02a652'>%s</font>" % (self.PROFICIENCY_SYMBOL * proficiencies, self.ABILITY_SYMBOL * abilities), self.GENESYS_SYMBOLS_STYLE) ]
+    def resolve_reference(self, item, table):
+        if isinstance(item, str):
+            result = { 'name': item }
+            result.update(table[item])
+            return result
+        else:
+            return item
+
+    def resolve_value(self, name, value, table, key):
+        if isinstance(value, dict):
+            return value
+        result = { key: value }
+        result.update(table[name])
+        return result
+
+    def format_skill(self, name, skill, data):
+        if skill == None:
+            skill = { 'rank': 0 }
+            skill.update(data['setting']['skills'][name])
+        else:
+            skill = self.resolve_value(name, skill, data['setting']['skills'], 'rank')
+        characteristic = skill['characteristic']
+        characteristic_rank = data['characteristics'][characteristic]
+        career = self.resolve_reference(data['career'], data['setting']['careers'])
+        is_career = name in career['skills']
+        proficiencies = min(skill['rank'], characteristic_rank)
+        abilities = max(skill['rank'], characteristic_rank) - proficiencies
+        format = "<b>%s</b>" if name in career["skills"] else "%s"
+        return[ Paragraph(format % name, self.SKILL_STYLE),
+          Paragraph(characteristic, self.SKILL_STYLE),
+          Paragraph("%d" % skill['rank'], self.SKILL_STYLE),
+          Paragraph("<font color='#fff201'>%s</font><font color='#02a652'>%s</font>"
+                    % (self.PROFICIENCY_SYMBOL * proficiencies, self.ABILITY_SYMBOL * abilities), self.GENESYS_SYMBOLS_STYLE) ]
 
     def skills(self, data):
         characteristics = data["characteristics"]
         return [
-            Table([ self.format_skill(name, int(skill["rank"]), characteristics[skill["characteristic"]], [])
-                    for (name, skill) in data["skills"].items() ]) ]
+            Table(
+                [["Skill","Char","Rank","Roll"]] +
+                    [ self.format_skill(name, data['skills'].get(name), data) for name in data["setting"]['skills'] ],
+                colWidths = (1.3*inch, 0.5*inch, 0.5*inch, 0.7*inch ),
+                rowHeights= [0.2*inch]*(1+len(data["setting"]['skills']))
+                ) ]
 
     def list_item(self, name, text):
         return Paragraph("<b>%s</b> %s" % (name, text), self.NORMAL_STYLE)
@@ -151,11 +189,40 @@ class CharacterSheet:
     def build_story(self, character_data):
         from reportlab.platypus import FrameBreak
         story = []
-        story.extend(self.title(character_data["name"], character_data['archetype']["name"], character_data['career']["name"]))
+        story.extend(self.title(character_data))
         story.extend(self.characteristics(character_data))
         story.append(FrameBreak())
         story.extend(self.skills(character_data))
         return story
+
+class FormTextField(Flowable):
+    '''A form field flowable'''
+
+    def __init__(self, xoffset=0, size=None, fillcolor=white, strokecolor=black):
+        from reportlab.lib.units import inch
+        if size is None: size = 4 * inch
+        self.fillcolor, self.strokecolor = fillcolor, strokecolor
+        self.xoffset = xoffset
+        self.size = size
+        # normal size is 4 inches
+        self.scale = size / (4.0 * inch)
+
+        def wrap(self, *args):
+            return (self.xoffset, self.size)
+
+        def draw(self):
+            canvas = self.canv
+            form = canvas.acroForm
+            form.textfield(name="fname", tooltip="Name",
+                           x=xoffset, y=0, borderStyle='inset')
+
+
+class CharacterForm(CharacterSheet):
+
+    def identity(self, character_data):
+        return Table(
+            [[Paragraph("Name"), FormTextField(size=10)]]
+        )
 
 class Page:
 
@@ -170,14 +237,15 @@ class Page:
 
 class LandscapePage(PageTemplate):
 
-    (PAGE_MARGIN_X, PAGE_MARGIN_Y) = (0.5*inch, 0.5*inch)
+    (PAGE_MARGIN_X, PAGE_MARGIN_Y) = (0.25*inch, 0.25*inch)
     (WIDTH, HEIGHT) = (11*inch, 8.5*inch)
     (CONTENT_WIDTH, CONTENT_HEIGHT) = (WIDTH - 2*PAGE_MARGIN_X, HEIGHT-2*PAGE_MARGIN_Y)
 
-    GUTTER_WIDTH = 0.5 * inch
-    LEFT_SIDE_WIDTH = 5*inch
-    RIGHT_SIDE_X = LEFT_SIDE_WIDTH + GUTTER_WIDTH
-    RIGHT_SIDE_WIDTH = CONTENT_WIDTH - RIGHT_SIDE_X
+    GUTTER_WIDTH = 0.25 * inch
+    LEFT_SIDE_WIDTH = 7.3*inch
+    RIGHT_SIDE_X = PAGE_MARGIN_X + LEFT_SIDE_WIDTH + GUTTER_WIDTH
+    RIGHT_SIDE_WIDTH = CONTENT_WIDTH + GUTTER_WIDTH - RIGHT_SIDE_X
+    print("left=%d, right=%d, total=%d" % (LEFT_SIDE_WIDTH, RIGHT_SIDE_WIDTH, LEFT_SIDE_WIDTH+RIGHT_SIDE_WIDTH))
 
     def __init__(self, character_sheet):
         self.character_sheet = character_sheet
@@ -185,10 +253,10 @@ class LandscapePage(PageTemplate):
 
         leftFrame = Frame(self.PAGE_MARGIN_X, self.PAGE_MARGIN_Y,
                           width = self.LEFT_SIDE_WIDTH, height = self.CONTENT_HEIGHT,
-                          id="main")
+                          id="main", showBoundary=True)
         rightFrame = Frame(self.RIGHT_SIDE_X, self.PAGE_MARGIN_Y,
-                           width = self.LEFT_SIDE_WIDTH, height = self.CONTENT_HEIGHT,
-                           showBoundary=False, id="sidebar")
+                           width = self.RIGHT_SIDE_WIDTH, height = self.CONTENT_HEIGHT,
+                           showBoundary=True, id="sidebar")
         # pictureFrame = Frame(self.back_offset + character_sheet.CARD_MARGIN, self.content_offset_y,
         #                      character_sheet.CONTENT_WIDTH, character_sheet.CONTENT_HEIGHT, showBoundary=False, id="pictureFrame")
 
@@ -202,7 +270,7 @@ class LandscapePage(PageTemplate):
         canvas.setFillColor(Color(100, 100, 100, alpha=0.7))
         canvas.rect(self.content_offset_x - self.character_sheet.CONTENT_MARGIN, self.content_offset_y + self.character_sheet.CONTENT_MARGIN,
                     self.PAGE_MARGIN_X + 2 * self.character_sheet.CONTENT_MARGIN,
-                    self.character_sheet.CONTENT_HEIGHT + 2 * card.CONTENT_MARGIN,
+                    self.character_sheet.CONTENT_HEIGHT + 2 * self.character_sheet.CONTENT_MARGIN,
                     stroke=False, fill=True)
 
         canvas.restoreState()
@@ -211,7 +279,8 @@ class PDFGenerator:
     # Generates the PDF by applying data to the page layout and card layout
 
     def generate(self, output, data_file, name, page_template_class=LandscapePage, character_sheet=CharacterSheet):
-        character_sheet = CharacterSheet()
+        #character_sheet = CharacterSheet()
+        character_sheet = CharacterForm()
         character_sheet.register_fonts()
         page_template_class = LandscapePage
 
@@ -227,25 +296,38 @@ class PDFGenerator:
         story.extend(character_sheet.build_story(data))
         doc.build(story)
 
-    def resolve_file(self, filename, default_directory=os.getcwd()):
+    DEFAULT_LOAD_DIRECTORIES = [os.getcwd(), os.path.dirname(__file__)]
+
+    def resolve_file(self, filename, default_directories=DEFAULT_LOAD_DIRECTORIES, ext='yaml'):
         if path.isfile(filename):
             return filename
-        if path.isfile(path.join(default_directory, filename)):
-            return path.join(default_directory, filename)
+        if not filename.endswith(('.yaml', '.YAML')):
+            filename = filename + '.yaml'
+        if path.isfile(filename):
+            return filename
+        for directory in default_directories:
+            if path.isfile(path.join(directory, filename)):
+                return path.join(directory, filename)
 
     def load_data(self, yamlfile, data_filter):
         """Read YAML file and return  2-dimensional list containing the data"""
 
         import yaml
 
+        load_directory = os.path.dirname(os.path.abspath(yamlfile))
         with open(yamlfile) as file:
             character_data = yaml.safe_load(file)
-            if (isinstance(character_data["setting"], basestring)):
-                character_data['setting'] = yaml.safeload(self.resolve_file(character_data['setting']))
+            if (isinstance(character_data["setting"], str)):
+                setting_path = self.resolve_file(character_data['setting'], [load_directory])
+                if path.isfile(setting_path):
+                    with open(setting_path) as setting_file:
+                        character_data['setting'] = yaml.safe_load(setting_file)
+                else:
+                    raise ValueError("Setting file %s does not exist" % (setting_path))
             return character_data
 
 parser = argparse.ArgumentParser(description="Generate Genesys character sheet PDF")
 args = parser.parse_args()
 
 generator = PDFGenerator()
-generator.generate("output/test.pdf", "data/brutus.yaml", "data/test.pdf")
+generator.generate("output/test.pdf", "data/alban_priest.yaml", "data/test.pdf")
